d9440bba1d5f63bbf37f8903503ebc65a3cb251d
Alazar <alazartefera028@gmail.com>
2025-06-06 11:50:12 +0300

    Aria added Die component Done

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,5 +1,15 @@
 export default function Die(props) {
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+    
     return (
-        <button>{props.value}</button>
+        <button 
+            style={styles}
+            onClick={props.hold}
+            aria-pressed={props.isHeld}
+            aria-label={`Die with value ${props.value}, 
+            ${props.isHeld ? "held" : "not held"}`}
+        >{props.value}</button>
     )
 }
